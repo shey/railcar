31fbdc5692d9aea75428be32c9cd32941b5b5960
shey <shey@shey.ca>
2023-10-31 22:19:34 -0400

    navbar with flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
-  def flash_classes(flash_type)
-    flash_base = "px-2 py-4 mx-auto font-sans font-medium text-center text-white"
+  def flash_classes(level)
+    flash_base = "py-2 px-2 mx-auto font-sans font-medium text-center text-white"
     {
       notice: "bg-indigo-600 #{flash_base}",
       error:  "bg-red-600 #{flash_base}",
-      alert: "bg-red-600 #{flash_base}"
-    }.stringify_keys[flash_type.to_s] || flash_type.to_s
+      alert: "bg-orange-600 #{flash_base}",
+      success: "bg-green-600 #{flash_base}",
+    }.stringify_keys[level.to_s] || level.to_s
   end
 end
